29a19e135fdc70ed4a9e885e9f50f65f01630017
SUJAL007-stack <sujalsolanki200621@gmail.com>
2024-09-03 12:31:34 +0530

    Add files via upload

---
+++ Function add & sub10.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int add(int a, int b)
+{
+    return a+b;
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    add(a,b);
+    cout<<"a and b:"<< endl;
+}
+int sub(int a, int b)
+{
+    return a-b;
+    cin>>a>>b;
+    sub(a,b);
+    cout<<"a and b:"<< endl;
+    return 0;
+}
